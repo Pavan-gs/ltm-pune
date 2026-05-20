aae266f402dadb1827027746c335cb96c7b1edb6
Pavan-gs <pavangs.info@gmail.com>
2026-05-20 16:29:20 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
 
@@ -59,7 +59,23 @@ print(f"Model Accuracy: {accuracy}")
 # -----------------------------------------
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# “CORS allows frontend applications running on different ports/domains to communicate with backend APIs.”
+
+'''FastAPI app becomes:
+
+Reusable ML Prediction Service
+
+“The ML service does not care whether requests come from Python, .NET, React, mobile apps, or other systems.”
+
+'''
 # -----------------------------------------
 # PREDICTION API
 # -----------------------------------------
@@ -92,3 +108,55 @@ def predict(data: dict):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+ '''Browser Swagger UI
+↓
+HTTP Request
+↓
+FastAPI Endpoint
+↓
+Prediction
+↓
+JSON Response
+“FastAPI follows OpenAPI standards to automatically generate API documentation.”
+“Backend APIs are usually tested independently before frontend integration.”'''
+
+'''FastAPI (app.py)
+
+Responsible for:
+
+ML prediction
+exposing API
+
+ .NET Backend (Program.cs)
+
+Responsible for:
+
+orchestration
+security
+validation
+calling ML service
+
+React (App.js)
+
+Responsible for:
+
+user interaction
+displaying predictions
+
+“Modern AI systems are usually composed of multiple small services communicating through APIs.”
+
+React Frontend (localhost:3000)
+            ↓
+.NET Backend API
+            ↓
+FastAPI ML Service (localhost:8000)
+            ↓
+ML Model'''
+
+'''“We retain FastAPI because our ML model is built in Python. 
+FastAPI exposes the model as a reusable API service that can be consumed by 
+enterprise applications like .NET backends and React frontends.”'''
+
+'''“FastAPI is one of the most commonly used frameworks for exposing Python AI/ML models as APIs. 
+It’s popular because it’s lightweight, fast, and integrates very well with modern AI workflows.”'''
