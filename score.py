3e74a34da951664f9900ee664a0cd358abb76c56
Pavan-gs <pavangs.info@gmail.com>
2026-05-25 17:13:11 +0530

    Add files via upload

---
+++ score.py
@@ -0,0 +1,55 @@
+import urllib.request
+import json
+
+# Request data goes here
+# The example below assumes JSON formatting which may be updated
+# depending on the format your endpoint expects.
+# More information can be found here:
+# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+
+data = {
+    "input_data": {
+        "columns": [
+            "CreditScore",
+            "Geography",
+            "Age",
+            "Tenure",
+            "Balance",
+            "NumOfProducts",
+            "HasCrCard",
+            "IsActiveMember",
+            "EstimatedSalary"
+        ],
+        "index": [0],
+        "data": [
+            [501, "France", 44, 4, 142051.1, 2, 0, 1, 74940.5]
+        ]
+    },
+    "params": {}
+}
+
+
+body = str.encode(json.dumps(data))
+
+url = 'https://pvnazml-uigws.centralindia.inference.ml.azure.com/score'
+# Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
+api_key = '2OnYzboNO8Q9tjZsPBHYofmYvDG7iQxTzgjoOC7A89KTCcC1AjVoJQQJ99CEAAAAAAAAAAAAINFRAZML3DPH'
+if not api_key:
+    raise Exception("A key should be provided to invoke the endpoint")
+
+
+headers = {'Content-Type':'application/json', 'Accept': 'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
